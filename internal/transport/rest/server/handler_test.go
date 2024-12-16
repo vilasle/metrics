245b29466c44caa4c3ceca8c12fc5b85ecd981e8
Vitaliy Slepchenko <almuert@yandex.ru>
2024-12-16 19:05:39 +0500

    change handler test for work with chi context

--- internal/transport/rest/server/handler_test.go
+++ internal/transport/rest/server/handler_test.go
@@ -12,210 +12,200 @@ import (
 )
 
 // FIXME if use chi router with url params when test will not work, because chi keep params on request context
-func TestUpdateMetric(t *testing.T) {
+func TestUpdateMetricAsTextPlain(t *testing.T) {
 	svc := server.NewStorageService(
 		memory.NewMetricGaugeMemoryRepository(),
 		memory.NewMetricCounterMemoryRepository(),
 	)
 
-	// cases := []struct {
-	// 	name        string
-	// 	path        []string
-	// 	method      string
-	// 	contentType []string
-	// 	statusCode  int
-	// }{
-	// 	{
-	// 		name:   "send normal gauge",
-	// 		method: http.MethodPost,
-	// 		path: []string{
-	// 			"/update/gauge/test/1.05",
-	// 			"/update/gauge/test1/1.033",
-	// 			"/update/gauge/test/140.10",
-	// 		},
-	// 		contentType: []string{
-	// 			"text/plain",
-	// 		},
-	// 		statusCode: http.StatusOK,
-	// 	},
-	// 	{
-	// 		name:   "send normal gauge with big value",
-	// 		method: http.MethodPost,
-	// 		path: []string{
-	// 			"/update/gauge/test/43435345435345.343424205",
-	// 			"/update/gauge/test1/4343534234634342.033",
-	// 			"/update/gauge/test/14000000000000.10",
-	// 		},
-	// 		contentType: []string{
-	// 			"text/plain",
-	// 		},
-	// 		statusCode: http.StatusOK,
-	// 	},
-	// 	{
-	// 		name:   "send a gauge with negative value",
-	// 		method: http.MethodPost,
-	// 		path: []string{
-	// 			"/update/gauge/test/-43435345435345.343424205",
-	// 			"/update/gauge/test1/-4343534234634342.033",
-	// 			"/update/gauge/test/-14000000000000.10",
-	// 		},
-	// 		contentType: []string{
-	// 			"text/plain",
-	// 		},
-	// 		statusCode: http.StatusOK,
-	// 	},
-	// 	{
-	// 		name:   "send gauge with string value",
-	// 		method: http.MethodPost,
-	// 		path: []string{
-	// 			"/update/gauge/test/string_value",
-	// 		},
-
-	// 		contentType: []string{
-	// 			"text/plain",
-	// 		},
-	// 		statusCode: http.StatusBadRequest,
-	// 	},
-	// 	{
-	// 		name:   "send gauge with empty value",
-	// 		method: http.MethodPost,
-	// 		path: []string{
-	// 			"/update/gauge/test/",
-	// 		},
-	// 		contentType: []string{
-	// 			"text/plain",
-	// 		},
-	// 		statusCode: http.StatusBadRequest,
-	// 	},
-	// 	{
-	// 		name:   "send normal counter",
-	// 		method: http.MethodPost,
-	// 		path: []string{
-	// 			"/update/counter/test/124",
-	// 			"/update/counter/test/12452",
-	// 			"/update/counter/test/213124",
-	// 			"/update/counter/test/0",
-	// 		},
-	// 		contentType: []string{
-	// 			"text/plain",
-	// 		},
-	// 		statusCode: http.StatusOK,
-	// 	},
-	// 	{
-	// 		name:   "send normal counter with big value",
-	// 		method: http.MethodPost,
-	// 		path: []string{
-	// 			"/update/counter/test/12400000000000",
-	// 			"/update/counter/test/1245200000000000000",
-	// 			"/update/counter/test/213124000000000000",
-	// 		},
-	// 		contentType: []string{
-	// 			"text/plain",
-	// 		},
-	// 		statusCode: http.StatusOK,
-	// 	},
-	// 	{
-	// 		name:   "send a counter with negative value",
-	// 		method: http.MethodPost,
-	// 		path: []string{
-	// 			"/update/counter/test/-124",
-	// 			"/update/counter/test/-12452",
-	// 			"/update/counter/test/-213124",
-	// 			"/update/counter/test/-0",
-	// 		},
-
-	// 		contentType: []string{
-	// 			"text/plain",
-	// 		},
-	// 		statusCode: http.StatusOK,
-	// 	},
-	// 	{
-	// 		name:   "send a counter with string value",
-	// 		method: http.MethodPost,
-	// 		path: []string{
-	// 			"/update/counter/test/sting_value",
-	// 		},
-	// 		contentType: []string{
-	// 			"text/plain",
-	// 		},
-	// 		statusCode: http.StatusBadRequest,
-	// 	},
-	// 	{
-	// 		name:   "send counter with empty value",
-	// 		method: http.MethodPost,
-	// 		path: []string{
-	// 			"/update/counter/test/",
-	// 			"/update/counter/test",
-	// 		},
-	// 		contentType: []string{
-	// 			"text/plain",
-	// 		},
-	// 		statusCode: http.StatusBadRequest,
-	// 	},
-	// 	{
-	// 		name:   "send unsupported type of metric",
-	// 		method: http.MethodPost,
-	// 		path: []string{
-	// 			"/update/another_metrics/test/",
-	// 		},
-	// 		contentType: []string{
-	// 			"text/plain",
-	// 		},
-	// 		statusCode: http.StatusBadRequest,
-	// 	},
-	// 	{
-	// 		name:   "send metric without name",
-	// 		method: http.MethodPost,
-	// 		path: []string{
-	// 			"/update/counter/",
-	// 			"/update/counter",
-	// 			"/update/gauge/",
-	// 			"/update/gauge",
-	// 		},
-	// 		contentType: []string{
-	// 			"text/plain",
-	// 		},
-	// 		statusCode: http.StatusNotFound,
-	// 	},
-	// 	{
-	// 		name:   "send without kind of metric",
-	// 		method: http.MethodPost,
-	// 		path: []string{
-	// 			"/update/",
-	// 		},
-	// 		contentType: []string{
-	// 			"text/plain",
-	// 		},
-	// 		statusCode: http.StatusBadRequest,
-	// 	},
-	// }
-
-	// for _, tt := range cases {
-	// t.Run(tt.name, func(t *testing.T) {
-	// for _, p := range tt.path {
-	rctx := chi.NewRouteContext()
-	rctx.URLParams.Add("type", "gauge")
-	rctx.URLParams.Add("name", "test")
-	rctx.URLParams.Add("value", "1.45")
-
-	// "/update/counter/test/-124",
-
-	req, err := http.NewRequest(http.MethodPost, "/update/{type}/{name}/{value}", nil)
-	if err != nil {
-		t.Fatal(err)
+	cases := []struct {
+		name        string
+		path        []map[string]string
+		method      string
+		contentType []string
+		statusCode  int
+	}{
+		{
+			name:   "send normal gauge",
+			method: http.MethodPost,
+			path: []map[string]string{
+				{"type": "gauge", "name": "test", "value": "1.05"},
+				{"type": "gauge", "name": "test1", "value": "1.033"},
+				{"type": "gauge", "name": "test", "value": "140.10"},
+			},
+			contentType: []string{
+				"text/plain",
+			},
+			statusCode: http.StatusOK,
+		},
+		{
+			name:   "send normal gauge with big value",
+			method: http.MethodPost,
+			path: []map[string]string{
+				{"type": "gauge", "name": "test", "value": "43435345435345.343424205"},
+				{"type": "gauge", "name": "test1", "value": "4343534234634342.033"},
+				{"type": "gauge", "name": "test", "value": "14000000000000.10"},
+			},
+			contentType: []string{
+				"text/plain",
+			},
+			statusCode: http.StatusOK,
+		},
+		{
+			name:   "send a gauge with negative value",
+			method: http.MethodPost,
+			path: []map[string]string{
+				{"type": "gauge", "name": "test", "value": "-43435345435345.343424205"},
+				{"type": "gauge", "name": "test1", "value": "-4343534234634342.033"},
+				{"type": "gauge", "name": "test", "value": "-14000000000000.10"},
+			},
+			contentType: []string{
+				"text/plain",
+			},
+			statusCode: http.StatusOK,
+		},
+		{
+			name:   "send gauge with string value",
+			method: http.MethodPost,
+			path: []map[string]string{
+				{"type": "gauge", "name": "test", "value": "string_value"},
+			},
+			contentType: []string{
+				"text/plain",
+			},
+			statusCode: http.StatusBadRequest,
+		},
+		{
+			name:   "send gauge with empty value",
+			method: http.MethodPost,
+			path: []map[string]string{
+				{"type": "gauge", "name": "test"},
+			},
+			contentType: []string{
+				"text/plain",
+			},
+			statusCode: http.StatusBadRequest,
+		},
+		{
+			name:   "send normal counter",
+			method: http.MethodPost,
+			path: []map[string]string{
+				{"type": "counter", "name": "test", "value": "124"},
+				{"type": "counter", "name": "test1", "value": "12452"},
+				{"type": "counter", "name": "test", "value": "213124"},
+			},
+			contentType: []string{
+				"text/plain",
+			},
+			statusCode: http.StatusOK,
+		},
+		{
+			name:   "send normal counter with big value",
+			method: http.MethodPost,
+			path: []map[string]string{
+				{"type": "counter", "name": "test", "value": "12400000000000"},
+				{"type": "counter", "name": "test1", "value": "1245200000000000000"},
+				{"type": "counter", "name": "test", "value": "213124000000000000"},
+			},
+			contentType: []string{
+				"text/plain",
+			},
+			statusCode: http.StatusOK,
+		},
+		{
+			name:   "send a counter with negative value",
+			method: http.MethodPost,
+			path: []map[string]string{
+				{"type": "counter", "name": "test", "value": "-124"},
+				{"type": "counter", "name": "test1", "value": "-12452"},
+				{"type": "counter", "name": "test", "value": "-213124"},
+				{"type": "counter", "name": "test", "value": "0"},
+			},
+			contentType: []string{
+				"text/plain",
+			},
+			statusCode: http.StatusOK,
+		},
+		{
+			name:   "send a counter with string value",
+			method: http.MethodPost,
+			path: []map[string]string{
+				{"type": "counter", "name": "test", "value": "sting_value"},
+			},
+			contentType: []string{
+				"text/plain",
+			},
+			statusCode: http.StatusBadRequest,
+		},
+		{
+			name:   "send counter with empty value",
+			method: http.MethodPost,
+			path: []map[string]string{
+				{"type": "counter", "name": "test"},
+			},
+			contentType: []string{
+				"text/plain",
+			},
+			statusCode: http.StatusBadRequest,
+		},
+		{
+			name:   "send unsupported type of metric",
+			method: http.MethodPost,
+			path: []map[string]string{
+				{"type": "another_metrics", "name": "test", "value": "1.05"},
+			},
+			contentType: []string{
+				"text/plain",
+			},
+			statusCode: http.StatusBadRequest,
+		},
+		{
+			name:   "send metric without name",
+			method: http.MethodPost,
+			path: []map[string]string{
+				{"type": "gauge"},
+				{"type": "counter"},
+			},
+			contentType: []string{
+				"text/plain",
+			},
+			statusCode: http.StatusNotFound,
+		},
+		{
+			name:   "send without kind of metric",
+			method: http.MethodPost,
+			path:   []map[string]string{},
+			contentType: []string{
+				"text/plain",
+			},
+			statusCode: http.StatusBadRequest,
+		},
 	}
-	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
-	// for _, ct := range tt.contentType {
-	// 	req.Header.Set("Content-Type", ct)
-	// }
-	rr := httptest.NewRecorder()
-	handler := UpdateMetric(svc)
-	handler.ServeHTTP(rr, req)
-	// if status := rr.Code; status != tt.statusCode {
-	// 	t.Errorf("handler returned wrong status code: got %v want %v",
-	// 		status, tt.statusCode)
-	// }
-	// }
-	// })
-	// }
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			for _, p := range tt.path {
+				ctx := chi.NewRouteContext()
+
+				for k, v := range p {
+					ctx.URLParams.Add(k, v)
+				}
+				req, err := http.NewRequest(http.MethodPost, "/update/{type}/{name}/{value}", nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+				req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+				for _, ct := range tt.contentType {
+					req.Header.Set("Content-Type", ct)
+				}
+				rr := httptest.NewRecorder()
+				handler := UpdateMetric(svc)
+				handler.ServeHTTP(rr, req)
+				if status := rr.Code; status != tt.statusCode {
+					t.Errorf("handler returned wrong status code: got %v want %v",
+						status, tt.statusCode)
+				}
+			}
+		})
+	}
 }
